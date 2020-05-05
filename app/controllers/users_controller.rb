ef0360428b5818d9532274c98e7b8c2470841490
sf <dvcktxty@gmail/com>
2020-05-05 06:03:59 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
+  
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  
+  # POST 
+  def create
+    # User.create(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      # Success (valid params)
+      flash[:success] = "Welcome to the Sample App!"
+      
+      # GET "/user/#{@user.id}"
+      redirect_to @user
+      
+      
+    else
+      # Failue (boy valid params)
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
