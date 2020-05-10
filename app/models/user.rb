f51396c90c9686f8ffac48b2d09c955da2952b66
sf <dvcktxty@gmail/com>
2020-05-10 04:07:43 +0000

    Finish ch13 expect ch13.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  
   attr_accessor :remember_token,
                 :activation_token,
                 :reset_token
@@ -72,6 +74,13 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", self.id)
+  end
+
 
   private
 
